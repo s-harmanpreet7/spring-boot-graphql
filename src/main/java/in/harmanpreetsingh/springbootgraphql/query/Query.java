3bb560a9c20c2e34ffdd8aac6240fa85af622ce2
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-12 00:18:20 +0530

    graphql query with json input

--- src/main/java/in/harmanpreetsingh/springbootgraphql/query/Query.java
+++ src/main/java/in/harmanpreetsingh/springbootgraphql/query/Query.java
@@ -1,6 +1,7 @@
 package in.harmanpreetsingh.springbootgraphql.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import in.harmanpreetsingh.springbootgraphql.request.Request;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class Query implements GraphQLQueryResolver {
         return "First Query";
     }
 
-    public String fullName(String firstName, String lastName) {
-        return firstName + " " + lastName;
+    public String fullName(Request request) {
+        return request.getFirstName() + " " + request.getLastName();
     }
 }
---
+++ src/main/java/in/harmanpreetsingh/springbootgraphql/request/Request.java
@@ -0,0 +1,11 @@
+package in.harmanpreetsingh.springbootgraphql.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Request {
+    private String firstName;
+    private String lastName;
+}
