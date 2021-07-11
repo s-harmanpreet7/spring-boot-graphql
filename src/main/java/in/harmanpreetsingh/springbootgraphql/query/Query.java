410ddea3bdfbdd059a45ed418c314c1e87658c8d
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-12 00:56:52 +0530

    added query and schema for fetching StudentResponse

--- src/main/java/in/harmanpreetsingh/springbootgraphql/query/Query.java
+++ src/main/java/in/harmanpreetsingh/springbootgraphql/query/Query.java
@@ -2,10 +2,16 @@ package in.harmanpreetsingh.springbootgraphql.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import in.harmanpreetsingh.springbootgraphql.request.Request;
+import in.harmanpreetsingh.springbootgraphql.response.StudentResponse;
+import in.harmanpreetsingh.springbootgraphql.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Query implements GraphQLQueryResolver {
+
+    @Autowired
+    private StudentService studentService;
 
     public String firstQuery() {
         return "First Query";
@@ -13,5 +19,9 @@ public class Query implements GraphQLQueryResolver {
 
     public String fullName(Request request) {
         return request.getFirstName() + " " + request.getLastName();
+    }
+
+    public StudentResponse getStudent(long id) {
+        return new StudentResponse(studentService.getStudentById(id));
     }
 }
