11853d89d8d9a5ae2c901bd0d1eaa9448dcfbd58
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-11 23:57:03 +0530

    test graphql query and schema

---
+++ src/main/java/in/harmanpreetsingh/springbootgraphql/query/Query.java
@@ -0,0 +1,16 @@
+package in.harmanpreetsingh.springbootgraphql.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+    public String firstQuery() {
+        return "First Query";
+    }
+
+    public String secondQuery() {
+        return "Second Query";
+    }
+}
